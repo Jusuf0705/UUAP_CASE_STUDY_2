0046d1eacb05fa6cf57d71d2888863cf3822623c
Jusuf0705 <jusufsalkanovic6@gmail.com>
2026-06-05 00:33:59 +0200

    Inicijalni commit analiza phishing e-poruka

--- main.py
+++ main.py
@@ -119,10 +119,6 @@ plt.tight_layout()
 plt.savefig('slika4.png', dpi=300, bbox_inches='tight')
 plt.show()
 
-# =============================================================================
-# 5. MODELI MAŠINSKOG UČENJA
-# =============================================================================
-
 X = df_full[feature_cols]
 y = df_full['is_phishing']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
